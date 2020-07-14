5461e78e216b2005004678d7efee10ec1be3d9e2
waristo <waristo.dev@gmail.com>
2020-07-14 15:54:09 +0900

    xlsx complte

--- data.py
+++ data.py
@@ -2,14 +2,32 @@ import os
 import numpy as np
 import pandas as pd
 
-dirList = os.listdir(os.getcwd())
+def makeDict(dirList):
+  attD = dict()
+  for folder in dirList :
+    if os.path.isdir(folder) and (folder.find(".") == -1) :
+      os.chdir(folder)
+      fileList = os.listdir(os.getcwd())
+      for f in fileList :
+        if (f.find(".new.xlsx") != -1) and (f.find("$") == -1):
+          excel = pd.read_excel(f)
+          for row in excel.itertuples() :
+            fn = str(row.파일명)
+            # ko = str(row.한국어)
+            en = str(row.영어)
+            if fn in attD :
+              # attD.get(fn)[0] + ko
+              attD.get(fn)[1] + en
+            else :
+              attD[fn] = ["", en]
+      os.chdir("..")
+  return attD
 
-for folder in dirList :
-  if os.path.isdir(folder) and (folder.find(".") == -1) :
-    os.chdir(folder)
-    fileList = os.listdir(os.getcwd())
-    for f in fileList :
-      if f.find(".xlsx") != -1 :
-        excel = pd.read_excel(f)
-    os.chdir("..")
+def addKorean(attD):
+  pass
+
+attD = makeDict(os.listdir(os.getcwd()))
+
+  
+print(attD)
     
