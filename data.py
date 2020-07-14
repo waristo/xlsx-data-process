856b5dc87fff0326df1164cd5498bda6c7adf650
waristo <waristo.dev@gmail.com>
2020-07-14 17:49:50 +0900

    except filenotexist

--- data.py
+++ data.py
@@ -2,29 +2,75 @@ import os
 import numpy as np
 import pandas as pd
 
+def readExcel(fileList, searchFile, attD):
+  for f in fileList :
+    if (f.find(searchFile) != -1) and (f.find("$") == -1):
+      excel = pd.read_excel(f)
+      for row in excel.itertuples() :
+        fn = str(row.파일명).replace("(","").replace(" ","_").replace(")","")
+        # ko = str(row.한국어)
+        en = str(row.영어)
+        if fn in attD :
+          # attD.get(fn)[0] += (" " + ko)
+          attD.get(fn)[1] += (" " + en)
+        else :
+          attD[fn] = ["", en]
+      attD = addKorean(attD, excel)
+  return attD
+
+def addKorean(attD, excel):
+  flag = True
+  for row in excel.itertuples() :
+    if flag :
+      flag = False  
+      prvFolder = row.대화번호
+      folder = "KOR_" + str(row.대화번호).rstrip().replace("(","").replace(" ","_").replace(")","")
+      os.chdir(folder)
+      prvFile = row.파일명
+      fileName = str(row.파일명).replace("(","").replace(" ","_").replace(")","")
+      try :
+        f = open(fileName + ".txt", 'rt')
+        attD.get(fileName)[0] = f.read()
+      except FileNotFoundError:
+        attD.get(fileName)[0] = "!!! FILE NOT FOUND !!!"
+      except UnicodeDecodeError:
+        f = open(fileName + ".txt", 'rt', encoding = 'UTF8')
+        attD.get(fileName)[0] = f.read()
+    if (prvFolder != row.대화번호) :
+      prvFolder = row.대화번호
+      folder = "KOR_" + str(row.대화번호).rstrip().replace("(","").replace(" ","_").replace(")","")
+      if folder != "KOR_nan" :
+        os.chdir("..")
+        os.chdir(folder)
+
+    if (prvFile != row.파일명) :
+      prvFile = row.파일명
+      fileName = str(row.파일명).replace("(","").replace(" ","_").replace(")","")
+      try :
+        f = open(fileName + ".txt", 'rt')
+        attD.get(fileName)[0] = f.read()
+      except FileNotFoundError:
+        attD.get(fileName)[0] = "!!! FILE NOT FOUND !!!"
+      except UnicodeDecodeError:
+        f = open(fileName + ".txt", 'rt', encoding = 'UTF16')
+        attD.get(fileName)[0] = f.read()
+    print(fileName)
+  os.chdir("..")
+  return attD
+
+
+
 def makeDict(dirList):
   attD = dict()
   for folder in dirList :
     if os.path.isdir(folder) and (folder.find(".") == -1) :
       os.chdir(folder)
       fileList = os.listdir(os.getcwd())
-      for f in fileList :
-        if (f.find(".new.xlsx") != -1) and (f.find("$") == -1):
-          excel = pd.read_excel(f)
-          for row in excel.itertuples() :
-            fn = str(row.파일명)
-            # ko = str(row.한국어)
-            en = str(row.영어)
-            if fn in attD :
-              # attD.get(fn)[0] + ko
-              attD.get(fn)[1] + en
-            else :
-              attD[fn] = ["", en]
+      attD = readExcel(fileList, ".new.xlsx", attD)
+
       os.chdir("..")
   return attD
 
-def addKorean(attD):
-  pass
 
 attD = makeDict(os.listdir(os.getcwd()))
 
